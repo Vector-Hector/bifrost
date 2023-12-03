df06f597b49dee5197162d0648ae5eed42609556
Vector-Hector <erik.doering@t-online.de>
2023-12-03 23:49:21 +0100

    added duplicate queue entry detection

--- dijkstra.go
+++ dijkstra.go
@@ -6,6 +6,7 @@ type dijkstraNode struct {
 	Arrival  uint64
 	Vertex   uint64
 	WalkTime uint32
+	Index    int // Index of the node in the heap
 }
 
 type priorityQueue []*dijkstraNode
@@ -22,6 +23,8 @@ func (pq *priorityQueue) Less(i, j int) bool {
 func (pq *priorityQueue) Swap(i, j int) {
 	l := *pq
 	l[i], l[j] = l[j], l[i]
+	l[i].Index = i
+	l[j].Index = j
 }
 
 func (pq *priorityQueue) Push(x interface{}) {
@@ -32,8 +35,15 @@ func (pq *priorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	x := old[n-1]
+	x.Index = -1 // for safety
 	*pq = old[:n-1]
 	return x
+}
+
+func (pq *priorityQueue) update(node *dijkstraNode, arrival uint64, targetWalkTime uint32) {
+	node.Arrival = arrival
+	node.WalkTime = targetWalkTime
+	heap.Fix(pq, node.Index)
 }
 
 func (r *RaptorData) StopTimesForKthTransfer(rounds *Rounds, current int) {
@@ -71,8 +81,11 @@ func (r *RaptorData) StopTimesForKthTransfer(rounds *Rounds, current int) {
 		delete(rounds.MarkedStopsForTransfer, stop)
 	}
 
+	nodeMap := make(map[uint64]*dijkstraNode)
+
 	for queue.Len() > 0 {
 		node := heap.Pop(&queue).(*dijkstraNode)
+		delete(nodeMap, node.Vertex)
 
 		arcs := r.StreetGraph[node.Vertex]
 		for _, arc := range arcs {
@@ -90,8 +103,6 @@ func (r *RaptorData) StopTimesForKthTransfer(rounds *Rounds, current int) {
 				continue
 			}
 
-			// todo handle shortcuts
-
 			next[arc.Target] = StopArrival{
 				Arrival:       arrival,
 				Trip:          TripIdTransfer,
@@ -101,11 +112,22 @@ func (r *RaptorData) StopTimesForKthTransfer(rounds *Rounds, current int) {
 			}
 			rounds.MarkedStops[arc.Target] = true
 			rounds.EarliestArrivals[arc.Target] = arrival
-			heap.Push(&queue, &dijkstraNode{
+
+			target, ok := nodeMap[arc.Target]
+			if ok {
+				queue.update(target, arrival, targetWalkTime)
+				continue
+			}
+
+			target = &dijkstraNode{
 				Arrival:  arrival,
 				Vertex:   arc.Target,
 				WalkTime: targetWalkTime,
-			})
+			}
+
+			nodeMap[arc.Target] = target
+
+			heap.Push(&queue, target)
 		}
 	}
 }
